6446f8d3cf26abcde17ddcecba2f6fcff1281752
Henrique <henriquechigumane@outlook.com>
2020-06-12 18:53:16 +0200

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -4,7 +4,7 @@ function onReady(callback) {
         window.clearInterval(intervalId);
         callback.call(this);
       }
-    }, 1000);
+    }, 1800);
   }
   
   function setVisible(selector, visible) {
