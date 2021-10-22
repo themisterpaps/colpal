ba70fae91ac834ed76d2a83f8bc7d83547e40c02
AlbertoYabeda <albertoacmacuacua@gmail.com>
2021-10-22 22:22:53 +0100

    fixed menu stuck on loading

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,3 @@
 function onReady(callback) {var intervalId = window.setInterval(function() {if (document.getElementsByTagName('body')[0] !== undefined) { window.clearInterval(intervalId); callback.call(this);      }
     }, 1800); }
-  onReady(function() {document.querySelector(".loading").classList.toggle(".hidden");});
+  onReady(function() {document.querySelector(".loading").classList.toggle("hidden"); });
