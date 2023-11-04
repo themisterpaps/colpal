f8f608df39cf5c4cc26a9376d1355a6152d576f5
Edmilson Rolas <edrolas.er@gmail.com>
2023-11-04 14:06:20 +0200

    base menu hamburguer

---
+++ menuhamburger.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+
+    menuToggle.addEventListener('click', function () {
+        menu.classList.toggle('active');
+    });
+});
